1f41d12289cb71ab236662a2e3014613ab3e5731
Yawo Echitey <yawo@echitey.com>
2019-12-18 16:22:29 -0500

    Initial Commit

---
+++ app/src/main/java/br/com/echitey/android/firebaseauthapp/RecoverPasswordActivity.java
@@ -0,0 +1,14 @@
+package br.com.echitey.android.firebaseauthapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecoverPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_password);
+    }
+}
